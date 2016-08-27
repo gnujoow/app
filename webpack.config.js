a9ea079a88cc2ee9ebc7f1a3a51bff815a6435c2
woojung <gnujoow.kim@gmail.com>
2016-08-28 03:30:15 +0900

    controller g home

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: "./frontend/index.jsx",
+  output: {
+    path: __dirname + "/app/assets/javascripts",
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: ['','.js','.jsx']
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx$/,
+      loader: "babel-loader",
+      query: {
+        presets: ['es2015', 'react']
+      }
+    }]
+  }
+};
